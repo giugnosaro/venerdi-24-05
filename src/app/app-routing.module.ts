b769f8ae2d67a183f79e6516b25407d16e2db7d7
giugnosaro <giugno_rosario@libero.it>
2024-05-24 14:32:28 +0200

    primo salvataggio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { Page404Component } from './pages/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent,
+    title:'Home'
+  },
+  {
+    path: 'fiat',
+    component: FiatComponent,
+    title:'Fiat'
+  },
+  {
+    path: 'ford',
+    component: FordComponent,
+    title:'Ford'
+  },
+  {
+    path: 'audi',
+    component: AudiComponent,
+    title:'Audi'
+  },
+  {
+    path: '**',
+    component: Page404Component,
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    FiatComponent,
+    FordComponent,
+    AudiComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
